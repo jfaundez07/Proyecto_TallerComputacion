5e4a2e6ef0fd5bce0a197aeca4a51a7eac61848b
javier-note <j.alcalde01@ufromail.cl>
2023-11-29 15:52:10 -0300

    ventana historial

--- app/src/main/java/com/example/proyectoapp2/MainActivity.kt
+++ app/src/main/java/com/example/proyectoapp2/MainActivity.kt
@@ -11,8 +11,6 @@ import java.io.IOException
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
