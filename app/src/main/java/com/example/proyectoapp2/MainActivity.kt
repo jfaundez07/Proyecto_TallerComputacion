9d5a406ff5069cfe12d927900481e307151654d4
javier-note <j.alcalde01@ufromail.cl>
2023-11-29 16:01:30 -0300

    ventana historial

--- app/src/main/java/com/example/proyectoapp2/MainActivity.kt
+++ app/src/main/java/com/example/proyectoapp2/MainActivity.kt
@@ -45,11 +45,17 @@ class MainActivity : AppCompatActivity() {
                 runOnUiThread {
                     if (datosSensoresList.size >= 10) {
                         binding.dato1.text = formatData(datosSensoresList[0])
+                        binding.dato1.textSize = 10f
                         binding.dato2.text = formatData(datosSensoresList[1])
+                        binding.dato2.textSize = 10f
                         binding.dato3.text = formatData(datosSensoresList[2])
+                        binding.dato3.textSize = 10f
                         binding.dato4.text = formatData(datosSensoresList[3])
+                        binding.dato4.textSize = 10f
                         binding.dato5.text = formatData(datosSensoresList[4])
+                        binding.dato5.textSize = 10f
                         binding.dato6.text = formatData(datosSensoresList[5])
+                        binding.dato6.textSize = 10f
                     } else {
                         binding.dato1.text = "No se encontraron suficientes datos"
                     }
