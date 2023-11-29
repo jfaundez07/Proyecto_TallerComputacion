b1538f8ea1045687be702ed5d5db110743ea3e21
Benttyman <jesustapiamartin@gmail.com>
2023-11-29 18:02:12 -0300

    Alerta

--- app/src/main/java/com/example/proyectoapp2/MainActivity.kt
+++ app/src/main/java/com/example/proyectoapp2/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.proyectoapp2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.example.proyectoapp2.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import okhttp3.*
@@ -50,30 +52,55 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 val jsonData = response.body?.string()
 
-                if (jsonData!= null) {
+
+                if (jsonData != null) {
                     val gson = Gson()
                     val datosSensores = gson.fromJson(jsonData, DatosSensores::class.java)
 
 
-                    val resultadoFormateado =   " Ultima lectura obtenida: \n" +
-                                                "\n"+
-                                                "-> Temperatura:   ${datosSensores.Temperatura} °C\n" +
-                                                "-> Humedad:        ${datosSensores.Humedad} %\n" +
-                                                "-> PM25:               ${datosSensores.PM25} [ug/m3]\n" +
-                                                "-> PM10:               ${datosSensores.PM10} [ug/m3]"
+                    val temperatura = datosSensores.Temperatura
 
-                    runOnUiThread {
-                        binding.tvResultado.text = resultadoFormateado
-                    }
 
-                } else {
-                    runOnUiThread {
-                        binding.tvResultado.text = "Error al analizar la respuesta del servidor"
+                    if (temperatura != null) {
+                        val resultadoFormateado =
+                            " Ultima lectura obtenida: \n" +
+                                    "\n" +
+                                    "-> Temperatura:   $temperatura °C\n" +
+                                    "-> Humedad:        ${datosSensores.Humedad} %\n" +
+                                    "-> PM25:               ${datosSensores.PM25} [ug/m3]\n" +
+                                    "-> PM10:               ${datosSensores.PM10} [ug/m3]"
+
+
+                        runOnUiThread {
+                            tvResultado.text = resultadoFormateado
+                        }
+
+
+                        if (temperatura >= 50) {
+                            val handler = Handler(Looper.getMainLooper())
+                            handler.post {
+                                mostrarAlerta("Temperatura Alta", "La temperatura es $temperatura °C")
+                            }
+                        }
+                    } else {
+                        runOnUiThread {
+                            tvResultado.text = "Error al analizar la respuesta del servidor"
+                        }
                     }
                 }
             }
         })
     }
+
+    private fun mostrarAlerta(titulo: String, mensaje: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(titulo)
+        builder.setMessage(mensaje)
+        builder.setPositiveButton("OK", null)
+        val dialog = builder.create()
+        dialog.show()
+    }
+
 
     data class DatosSensores(
         val Temperatura: Double?,
